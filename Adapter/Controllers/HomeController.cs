f581a5b9e14a8b9414a964a4471586dbb13e7042
ethgvnt <ethgvnt@IN-00208058>
2019-04-28 03:09:21 +0530

    Quiz controller code - updated

--- Adapter/Controllers/HomeController.cs
+++ Adapter/Controllers/HomeController.cs
@@ -10,7 +10,19 @@ namespace Adapter.Controllers
     {
         public ActionResult Index()
         {
+
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(User user)
+        {
+            user.Id = new Guid("9B91BFBC-CFE8-48ED-8D61-5E2E53332B6A");
+
+            Session["user"] = user;
+
+            return RedirectToAction("Index", "Quiz");
+            // return View();
         }
 
         public ActionResult About()
--- Adapter/Controllers/QuizController.cs
+++ Adapter/Controllers/QuizController.cs
@@ -5,6 +5,8 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Practices.EnterpriseLibrary.Caching;
+using Microsoft.Practices.EnterpriseLibrary.Caching.Expirations;
 
 namespace Adapter.Controllers
 {
@@ -14,46 +16,104 @@ namespace Adapter.Controllers
         private static string CONNECTION_STRING = ConfigurationManager.ConnectionStrings["Adapters"].ToString();
 
         // GET: Quiz
-        public ActionResult Index()
+        public ActionResult Index(bool isFromPost = false)
         {
-            var quiz = GetQuizRecords();
+            QuestionOptions questions = new QuestionOptions();
+            if (!isFromPost)
+            {
+                var quiz = GetQuizRecords();
 
-            var questions = GetQuizQuestion(quiz.Id);
+                questions = GetQuizQuestion(quiz.Id);
 
+                questions.ElapseTime = quiz.ElapseTime;
+
+            }
+            else
+            {
+                var quizList = (List<QuestionOptions>)Session["Questions"];
+
+                questions = quizList.Where(x => x.IsAnswered == false).FirstOrDefault();
+            }
             return View(questions);
         }
 
-        private static QuestionOptions GetQuizQuestion(Guid quizId)
+        [HttpPost]
+        public ActionResult Index(QuestionOptions options, bool chkCorrectAnswer)
         {
-            QuestionOptions quiz = new QuestionOptions();
+            if (chkCorrectAnswer)
+            {
+                var user = (User)Session["user"];
+                user.Score = user.Score + 5;
+                Session["user"] = user;
+            }
 
+            var quizList = (List<QuestionOptions>)Session["Questions"];
+            int index = quizList.FindIndex(a => a.Id == options.Id);
+            var quiz = quizList[index];
+            quiz.IsAnswered = true;
+            quizList[index] = quiz;
+            Session["Questions"] = quizList;
+
+
+
+            return RedirectToAction("Index", new { isFromPost = true });
+        }
+
+        private QuestionOptions GetQuizQuestion(Guid quizId)
+        {
+            List<QuestionOptions> quizList = new List<QuestionOptions>();
+            QuestionOptions quiz = new QuestionOptions();
             SqlConnection connection = new SqlConnection(CONNECTION_STRING);
             var date = DateTime.Now.ToString("dd/MM/yyyy hh:mm");
 
             SqlCommand command = new SqlCommand(
           @"select question.Id,question.Description,questionoptions.Description as OptionDescription,IsAnswer from question 
 inner join quizquestions  on question.id = quizquestions.QuestionId
-INNER JOIN questionoptions on questionoptions.QuestionId = quizquestions.QuestionId  WHERE QuizId='" + quizId + "' AND QuizQuestions.IsPublished=0",
+INNER JOIN questionoptions on questionoptions.QuestionId = quizquestions.QuestionId  WHERE QuizId='" + quizId + "' AND QuizQuestions.IsPublished=0" +
+"Order by question.Id",
           connection);
             connection.Open();
 
             SqlDataReader reader = command.ExecuteReader();
 
-            List<Options> options = new List<Options>();
+            //Guid prevId = Guid.Empty;
+            //List<Options> options = new List<Options>();
             if (reader.HasRows)
             {
                 while (reader.Read())
                 {
-                    quiz.Id = new Guid(Convert.ToString(reader["Id"]));
-                    quiz.Description = Convert.ToString(reader["Description"]);
+                    var guid = new Guid(Convert.ToString(reader["Id"]));
 
-                    options.Add(new Options { Description = Convert.ToString(reader["OptionDescription"]), IsAnswer = Convert.ToBoolean(reader["IsAnswer"]) });
+                    if (quizList.Where(x => x.Id == guid).FirstOrDefault() == null)
+                    {
+                        quiz = new QuestionOptions();
+                    }
+
+                    //if (prevId != guid)
+                    //{
+                    //    if (prevId != Guid.Empty)
+                    //    {
+                    //        quizList.Add(quiz);
+                    //    }
+                    //    quiz = new QuestionOptions();
+                    //}
+                    //prevId = guid;
+                    quiz.Id = guid;
+                    quiz.Description = Convert.ToString(reader["Description"]);
+                    if (quiz.OptionsList == null)
+                    {
+                        quiz.OptionsList = new List<Options>();
+                    }
+                    quiz.OptionsList.Add(new Options { Description = Convert.ToString(reader["OptionDescription"]), IsAnswer = Convert.ToBoolean(reader["IsAnswer"]) });
+
+                    if (quizList.Where(x => x.Id == guid).FirstOrDefault() == null)
+                    {
+                        quizList.Add(quiz);
+                    }
+
                 }
             }
-            else
-            {
-                Console.WriteLine("No rows found.");
-            }
+
             reader.Close();
 
             if (quiz.Id != Guid.Empty)
@@ -61,12 +121,12 @@ INNER JOIN questionoptions on questionoptions.QuestionId = quizquestions.Questio
                 //  UpdateQuestionPublish(quiz.Id, quizId);
             }
 
-            quiz.OptionsList = options;
-
-            return quiz;
+            // quiz.OptionsList = options;
+            Session["Questions"] = quizList;
+            return quizList.FirstOrDefault();
         }
 
-        private static int UpdateQuestionPublish(Guid quizQuestionId, Guid quizId)
+        private int UpdateQuestionPublish(Guid quizQuestionId, Guid quizId)
         {
             SqlConnection connection = new SqlConnection(CONNECTION_STRING);
             var date = DateTime.Now.ToString("dd/MM/yyyy hh:mm");
@@ -81,7 +141,7 @@ INNER JOIN questionoptions on questionoptions.QuestionId = quizquestions.Questio
         }
 
 
-        private static Quiz GetQuizRecords()
+        private Quiz GetQuizRecords()
         {
             Quiz quiz = new Quiz();
 
@@ -128,18 +188,24 @@ INNER JOIN questionoptions on questionoptions.QuestionId = quizquestions.Questio
         public DateTime StartTime { get; set; }
         public int ElapseTime { get; set; }
         public List<Options> OptionsList { get; set; }
+
+        public bool IsAnswered { get; set; }
     }
 
     public class Options
     {
         public string Description { get; set; }
         public bool IsAnswer { get; set; }
+
     }
 
     public class User
     {
+        public Guid Id { get; set; }
         public string Email { get; set; }
+        public string Password { get; set; }
 
+        public int Score { get; set; }
     }
 
 }
